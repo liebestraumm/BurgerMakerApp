8d03cfe27a37f7e831818661bbca807ebe44ee02
Carlos <carlosarturo.agelvisvergara@connect.qut.edu.au>
2020-05-08 15:32:51 +1000

    Adding containers and components files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import Layout from './components/Layout/Layout';
+import classes from './App.css';
+import BurgerMaker from './containers/BurgerMaker/BurgerMaker';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className={classes.App}>
+      <Layout>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <BurgerMaker />
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </Layout>
     </div>
   );
 }
---
+++ src/containers/BurgerMaker/BurgerMaker.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import Aux from "../../hoc/Aux";
+class BurgerMaker extends Component {
+    render(){
+        return(
+            <Aux>
+                <div>Burger</div>
+                <div>BuildControl</div>
+            </Aux>
+        );
+    }
+}
+
+export default BurgerMaker;
